097a5595c0f19d03e8e4e0f206851dabde5ebdf5
Mohamed Hassan <mhamedhassan109@gmail.com>
2025-09-24 03:18:08 +0300

    side bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import Header from "./Componens/Header/Header"
+import SideBar from "./Componens/SideBar/SideBar"
 import Hero from "./Componens/Hero/Hero"
 import About from "./Componens/About/About"
 import Projects from "./Componens/Projects/Projects"
 import Skills from "./Componens/Skills/Skills"
 import Footer from "./Componens/Footer/Footer"
 import Main from "./Styles/Main.scss"
-import {Container} from "react-bootstrap"
+import {Col, Container, Row} from "react-bootstrap"
 
 function App() {
   return (
     <div className="App">
-      <Container>
-              <Header/>
+     
+
+        <Row>
+          <Col sm="3">
+           <SideBar/>
+           </Col>
+
+
+          <Col sm="9">
               <Hero/>
               <About/>
               <Projects/>
               <Skills/>
               <Footer/>
-     </Container>
+          </Col>
+        </Row>
+             
+    
 
     </div>
   );
---
+++ src/Componens/SideBar/SideBar.jsx
@@ -0,0 +1,40 @@
+
+
+function SideBar() {
+  return (
+    <div className='SideBar'>
+      <div className="imgPortflio">
+       
+      </div>
+
+      <h3>mohamed hassan </h3>
+      <h6>front end devoloper</h6>
+
+      <div className="socialMedia">
+        <a href="" target="blank"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https://web.whatsapp.com/" target="blank"><i class="fa-brands fa-whatsapp"></i></a>
+        <a href="https://www.linkedin.com/in/mohamed-hassan-57209b266/?trk=li_LOL_DA_global_careers_jobsgtm_otwGeneral_res_Sep2023_dav2" target="blank"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://github.com/mhamedhassan109" target="blank"><i class="fa-brands fa-github"></i></a>
+       
+       
+      
+       
+       
+      </div>
+
+      <div className="navbar">
+        <ul>
+          <li><i class="fa-solid fa-house"></i>home</li>
+          <li><i class="fa-solid fa-user"></i>About</li>
+          <li><i class="fa-solid fa-file"></i>resume</li>
+          <li><i class="fa-solid fa-photo-film"></i>portfolio</li>
+          <li><i class="fa-solid fa-server"></i>serveice</li>
+          <li><i class="fa-solid fa-address-card"></i>contact</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default SideBar
